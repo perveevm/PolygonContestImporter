d6222c9bb934845d57b5f5a1ef66a5bc497e3a5d
ilsaf13 <ilsaf13@yahoo.com>
2016-11-09 10:19:35 +0300

    Revert "Attachments, groups.txt, preliminary testset not shown"
    This reverts commit 154e4495a8e010878a7198d455a244c1767e164c.

--- src/pcms2/Problem.java
+++ src/pcms2/Problem.java
@@ -7,11 +7,7 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -20,51 +16,40 @@ import java.util.TreeSet;
  */
 public class Problem {
     String XMLpath;
-    String GroupsPath;
     String ID;
     String ScriptType;
     String Name;
     String shortName;
     String url;
     Testset[] testsets;
-    ArrayList <Attachment> attachments;
     Verifier v;
     boolean hasPreliminary = false;
 
     public Problem(String path, String idprefix, String type) throws Exception {
-        XMLpath = path + "/problem.xml";
-        GroupsPath = path + "/files/groups.txt";
+        XMLpath = path;
         ID = idprefix;
         ScriptType = type;
-        if (ScriptType.equals("ioi")){
+        if (ScriptType.equals("ioi")) {
             testsets = new Testset[2];
         } else {
             testsets = new Testset[1];
         }
         parse();
     }
+
     public void parse() throws Exception {
         System.out.println("parsing problem...");
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document doc = builder.parse(XMLpath);
-        BufferedReader groupstxt = null;
-        if ((new File(GroupsPath)).exists()) {
-            groupstxt = new BufferedReader(new FileReader(GroupsPath));
-        }
         //NodeList problem = doc.getDocumentElement().getChildNodes();
         Element el = doc.getDocumentElement();
         shortName = el.getAttribute("short-name");
-        url = el.getAttribute("url");
-        if (ID.startsWith("com.codeforces.polygon")){
-            String[] t = url.split("/");
-            ID = "com.codeforces.polygon." + t[t.length - 2];
-        }
         ID = ID + "." + shortName;
-
+        url = el.getAttribute("url");
 
         //names
-        NodeList nl = ((Element)doc.getElementsByTagName("names").item(0)).getElementsByTagName("name");
+        NodeList nl = ((Element) doc.getElementsByTagName("names").item(0)).getElementsByTagName("name");
         for (int i = 0; i < nl.getLength(); i++) {
             Node n = nl.item(i);
             el = (Element) n;
@@ -76,7 +61,7 @@ public class Problem {
             }
         }
         //judging
-        el = (Element)doc.getElementsByTagName("judging").item(0);
+        el = (Element) doc.getElementsByTagName("judging").item(0);
         String inp = el.getAttribute("input-file");
         if (inp.isEmpty()) inp = "*";
         String outp = el.getAttribute("output-file");
@@ -151,42 +136,12 @@ public class Problem {
                     } else {
                         gmap.add(g);
                         Group gg = new Group();
-
-                        if (groupstxt != null) {
-                            String[] group_params = groupstxt.readLine().trim().split("\t");
-
-                            for (int ig = 0; ig < group_params.length; ig++) {
-                                String[] kv = getKeyAndValue(group_params[ig]);
-                                if (kv[0].equals("group")){
-                                    if (Integer.parseInt(kv[1]) != ts.groups.size()) {
-                                        System.out.println("WARNING: Group numbers are not consecutive?");
-                                    }
-                                } else if (kv[0].equals("group-bonus")) {
-                                    gg.groupBonus = kv[1];
-                                } else if (kv[0].equals("require-groups")) {
-                                    String[] grps = kv[1].split(" ");
-                                    gg.requireGroups = "";
-                                    for (String grp:grps) {
-                                        try{
-                                            int abc = Integer.parseInt(grp);
-                                            abc++;
-                                            gg.requireGroups += "" + abc + " ";
-                                        } catch (NumberFormatException e) {
-                                            continue;
-                                        }
-                                    }
-                                    //gg.requireGroups = kv[1];
-                                } else {
-                                    System.out.println("WARNING: unknown parameter in groups.txt");
-                                }
-                            }
-                        }
                         gg.first = j;
                         gg.last = j;
                         gg.comment = g;
                         ts.groups.add(gg);
                     }
-                } else if (hasGroups){
+                } else if (hasGroups) {
                     System.out.println("WARNING: Groups are enabled but test '" + j + "' has no group!");
                 }
 
@@ -204,48 +159,7 @@ public class Problem {
             }
             testsets[0].Tests = temp;
         }
-        //files attachments
-        el = (Element)((Element)doc.getElementsByTagName("files").item(0)).getElementsByTagName("attachments").item(0);
-        attachments = new ArrayList<>();
-        if (el != null) {
-            nl = el.getElementsByTagName("file");
-
-            for (int i = 0; i < nl.getLength(); i++) {
-                el = (Element) nl.item(i);
-                String atpath = el.getAttribute("path");
-                String ext = atpath.substring(atpath.lastIndexOf('.') + 1, atpath.length());
-                String fname = atpath.substring(atpath.lastIndexOf("/") + 1, atpath.lastIndexOf('.'));
-                System.out.println("File name is '" + fname + "'");
-                if (fname.equals(shortName) && !ext.equals("h")) {
-                    System.out.println("Skipping solution stub '" + fname + "." + ext + "'");
-                    continue;
-                }
-                Attachment attach = new Attachment();
-                attach.href = atpath;
-                if (ext.equals("h")) {
-                    if (fname.endsWith("_c")) {
-                        attach.languageId = "c.gnu";
-                    } else {
-                        attach.languageId = "cpp.gnu";
-                    }
-                } else if (ext.equals("cpp")) {
-                    attach.languageId = "cpp.gnu";
-                } else if (ext.equals("c")) {
-                    attach.languageId = "c.gnu";
-                } else if (ext.equals("pas")) {
-                    attach.languageId = "pascal.free";
-                } else if (ext.equals("java")) {
-                    attach.languageId = "java";
-                } else {
-                    attach = null;
-                }
-                if (attach != null) {
-                    attachments.add(attach);
-                }
-            }
-        }
-        //assets (checker)
-        el = (Element)((Element)doc.getElementsByTagName("assets").item(0)).getElementsByTagName("checker").item(0);
+        el = (Element) ((Element) doc.getElementsByTagName("assets").item(0)).getElementsByTagName("checker").item(0);
         v = new Verifier();
         v.type = el.getAttribute("type");
         el = (Element) el.getElementsByTagName("binary").item(0);
@@ -254,7 +168,7 @@ public class Problem {
         //shortName = problem.getAttributes().getNamedItem("short-name").getNodeValue();
     }
 
-    public void print(PrintWriter pw){
+    public void print(PrintWriter pw) {
         pw.println("<?xml version = \"1.0\" encoding=\"UTF-8\"?>");
         pw.println("<problem");
         pw.println("\tversion = \"1.0\"");
@@ -262,23 +176,12 @@ public class Problem {
         pw.println(">");
         pw.println("\t<judging>");
         pw.println("\t\t<script type = \"%" + ScriptType + "\">");
-        for (int i = 0; i < testsets.length; i++){
+        for (int i = 0; i < testsets.length; i++) {
             testsets[i].print(pw, "\t\t\t", ScriptType);
         }
         v.print(pw, "\t\t\t");
-        for (Attachment at:attachments){
-            at.print(pw, "\t\t\t");
-        }
         pw.println("\t\t</script>");
         pw.println("\t</judging>");
         pw.println("</problem>");
-    }
-    String[] getKeyAndValue(String s){
-        //key="value"
-        String[] ss = s.split("=");
-        ss[0] = ss[0].trim();
-        ss[1] = ss[1].trim();
-        ss[1] = ss[1].substring(1, ss[1].length() - 1);
-        return ss;
     }
 }
