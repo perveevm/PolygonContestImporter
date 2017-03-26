403cca2cbc1927d22c1e89a49df105e7e0c9b03c
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2017-03-26 23:03:51 +0300

    run.sh modified to use lib/*

--- deploy/run.sh
+++ deploy/run.sh
@@ -5,4 +5,4 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-java -cp $DIR/polygon.jar pcms2.Main $*
+java -cp $DIR/polygon.jar:$DIR/../lib/* pcms2.Main $*
