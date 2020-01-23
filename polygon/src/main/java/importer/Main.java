4751f7c3070c962fcef668fac1e35af949dd7db6
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2020-01-23 20:46:08 +0300

    version bump 1.2-beta-3

--- polygon/src/main/java/importer/Main.java
+++ polygon/src/main/java/importer/Main.java
@@ -14,7 +14,7 @@ import java.util.concurrent.Callable;
             DownloadContest.class
         },
         mixinStandardHelpOptions = true,
-        version = "PolygonContestImporter 1.2"
+        version = "PolygonContestImporter 1.2-beta-3"
 )
 public class Main implements Callable<Void> {
 
