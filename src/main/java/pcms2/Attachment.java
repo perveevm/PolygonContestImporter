f802a486ce16fdd9207c37455654ef1e9deadd34
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2020-03-11 00:43:37 +0300

    fixed extension and basename splitting

--- src/main/java/pcms2/Attachment.java
+++ src/main/java/pcms2/Attachment.java
@@ -1,5 +1,7 @@
 package pcms2;
 
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Properties;
@@ -11,6 +13,11 @@ public class Attachment {
     String href;
     String languageId;
 
+    private Attachment(String href, String languageId) {
+        this.href = href;
+        this.languageId = languageId;
+    }
+
     public void print(PrintWriter pw, String tabs){
         pw.println(tabs + "<attachment language-id=\"" + languageId + "\" href=\"" + href + "\"/>");
 
@@ -19,8 +26,9 @@ public class Attachment {
         ArrayList<Attachment> result = new ArrayList<>();
         for (polygon.Attachment attachment : attachments) {
             String atpath = attachment.getPath();
-            String ext = atpath.substring(atpath.lastIndexOf('.') + 1, atpath.length());
-            String fname = atpath.substring(atpath.lastIndexOf("/") + 1, atpath.lastIndexOf('.'));
+            String atname = FilenameUtils.getName(atpath);
+            String ext = FilenameUtils.getExtension(atname);
+            String fname = FilenameUtils.getBaseName(atname);
             //System.out.println("DEBUG: File name is '" + fname + "'");
             if (fname.equals("Solver") || (fname.equals(shortName) && !ext.equals("h"))) {
                 System.out.println("Skipping solution stub '" + fname + "." + ext + "'");
@@ -30,11 +38,8 @@ public class Attachment {
             if (languagesProps.getProperty(ext) != null) {
                 String[] langs = languagesProps.getProperty(ext).split(",");
 
-                for (int i = 0; i < langs.length; i++) {
-                    Attachment attach = new Attachment();
-                    attach.href = atpath;
-                    attach.languageId = langs[i].trim();
-                    result.add(attach);
+                for (String lang : langs) {
+                    result.add(new Attachment(atpath, lang.trim()));
                 }
             }
         }
