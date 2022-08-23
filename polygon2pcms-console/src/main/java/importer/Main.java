4dc6ddbb0c60b2e76baa7f0954c13afbabe796fe
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2022-08-23 18:26:27 +0300

    Bump version to 1.4.1-SNAPSHOT

--- polygon2pcms-console/src/main/java/importer/Main.java
+++ polygon2pcms-console/src/main/java/importer/Main.java
@@ -14,7 +14,7 @@ import java.util.concurrent.Callable;
             DownloadContest.class
         },
         mixinStandardHelpOptions = true,
-        version = "PolygonContestImporter 1.4.0"
+        version = "PolygonContestImporter 1.4.1-SNAPSHOT"
 )
 public class Main implements Callable<Void> {
 
